cc86fc0f6f117b6fc284d600204872e33a5b1857
Daniel Diaz <diaz.carrete@facebook.com>
2020-01-20 00:03:52 +0100

    ...

--- src/main/java/org/danisoft/App.java
+++ src/main/java/org/danisoft/App.java
@@ -1,5 +1,7 @@
 package org.danisoft;
 
+import java.util.Date;
+
 /**
  * Hello world!
  *
@@ -9,5 +11,9 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+    }
+
+    public static void arghArgs(Date date, int i, int j, boolean boo, boolean baa, String stuff) {
+
     }
 }
